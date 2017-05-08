6ab2f012f33312d7836cd69f133c2f58d9fd28a0
Max <max.mjm.melo@gmail.com>
2017-05-07 23:20:13 -0400

    arquivo principal

---
+++ testador.sh
@@ -0,0 +1,25 @@
+#!/bin/bash   
+
+clear
+
+PROGRAM = "main"
+EXT = ".c"
+
+TESTS = "test1.txt test2.txt"
+
+
+function runTests{
+	select opt in $TESTS; do
+		#parei aqui
+		#TODO ler arquivo de texto
+		#TODO executar programa e redirecionar fluxo para arquivo especificado
+		if [ "$opt" =  ]
+	done
+}
+
+
+
+
+echo "Olá ${USER}, verificador te testes para programas!"
+
+
